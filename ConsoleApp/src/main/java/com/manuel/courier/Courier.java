923fdc76cac196ccaed7ee505f32d6a8a2833004
Manuel-Peluso <manuelpeluso02@gmail.com>
2024-01-28 17:43:52 +0100

    Creato classe Courier

---
+++ ConsoleApp/src/main/java/com/manuel/courier/Courier.java
@@ -0,0 +1,45 @@
+package com.manuel.courier;
+
+public class Courier {
+	
+	int id;
+	String nome;
+	String url;
+	
+	public Courier() {
+		super();
+	}
+
+	public Courier(int id, String nome, String url) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.url = url;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "Courier [id=" + id + ", nome=" + nome + ", url=" + url + "]";
+	}
+	
+
+}
