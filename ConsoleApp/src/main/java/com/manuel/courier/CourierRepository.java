80369cc2e1457353ac4614c4d11ccdfa8d438764
Manuel-Peluso <manuelpeluso02@gmail.com>
2024-01-28 18:09:16 +0100

    Aggiunto metodi per l'aggiunta e la rimozione del corriere

--- ConsoleApp/src/main/java/com/manuel/courier/CourierRepository.java
+++ ConsoleApp/src/main/java/com/manuel/courier/CourierRepository.java
@@ -1,7 +1,31 @@
 package com.manuel.courier;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CourierRepository {
 
+	private List<Courier> listCourier = new ArrayList<Courier>();
 	
+	public List<Courier> getCouriers() {
+		return listCourier;
+	}
+	
+	public void addCourier(Courier courier) {
+		listCourier.add(courier);		
+	}
+	
+	public boolean removeCourier(int id) {
+		listCourier.remove(getCourierById(id));
+		return getCourierById(id) != null;
+	}
+
+	public Courier getCourierById(int id) {
+		Courier courier = getCouriers().stream()
+				.filter(c -> c.getId() == id)
+				.findFirst()
+				.orElse(null);
+		return courier;
+	}
 	
 }
